a00797b4da3d75af216fa945fca446f6c0a7880e
sandeepbasva-apalya <sandeep.b@apalya.com>
2020-02-08 20:40:36 +0530

    App level templates and Bootstrap integration

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,29 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        "author": "Sandeep",
+        'title': "Blog Post1",
+        'content': "First content",
+        'date_posted': "Jan 1, 2020"
+    },
+    {
+        "author": "Basva",
+        'title': "Blog Post2",
+        'content': "Second content",
+        'date_posted': "Jan 10  , 2020"
+    }
+]
+
 
 def home(request):
-    return HttpResponse("<h1>Home Page</h1>")
+    context = {
+        "posts": posts
+    }
+    return render(request, 'blog/home.html', context=context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', context={"title": "Latest"})
 
