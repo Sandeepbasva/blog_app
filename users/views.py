2b6204992617df2aa5cf9f0d938f37e7229ee136
sandeepbasva-apalya <sandeep.b@apalya.com>
2020-02-16 14:13:35 +0530

    Added Login, Logout, Profile page, Authorization based pages

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 # Create your views here.
 
@@ -11,10 +12,15 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, 'Account created for {}!'.format(username))
-            return redirect('blog-home')
+            messages.success(request, 'Your account {} has been created, Please login!'.format(username))
+            return redirect('login')
     else:
         form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
 
